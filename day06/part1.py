9d22f4b0dc7bc1c92d8e173d7f77e24c70ee139d
Stanislav Khrapov <khrapovs@gmail.com>
2022-12-06 11:08:43 +0100

    Day 6 solution

---
+++ day06/part1.py
@@ -0,0 +1,26 @@
+with open("data.txt", mode="r") as f:
+    signals = f.read().split("\n")
+
+MARKER_LEN = 4
+
+
+def detect_marker(signal: str) -> int:
+    nunique, pos = 0, 0
+    while (pos + MARKER_LEN <= len(signal)) and (nunique < MARKER_LEN):
+        nunique = len(set(signal[pos : pos + MARKER_LEN]))
+        pos += 1
+    return pos + MARKER_LEN - 1
+
+
+def detect_marker2(signal: str) -> int:
+    for pos in range(len(signal) - MARKER_LEN):
+        marker = signal[pos : pos + MARKER_LEN]
+        if len(set(marker)) == MARKER_LEN:
+            return pos + MARKER_LEN
+        else:
+            continue
+
+
+answer = [detect_marker(signal) for signal in signals]
+
+print(answer)
